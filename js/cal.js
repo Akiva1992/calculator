60f198c83d7b94b01689f8f3929cde184eddee4b
Akiva1992 <breuerakiva@gmail.com>
2022-09-07 20:58:51 +0300

    created files folders and created the first 4 basic functions

---
+++ js/cal.js
@@ -0,0 +1,11 @@
+const add = (num1, num2) => num1+num2;
+const subtract = (num1, num2) => num1-num2;
+const multiply = (num1, num2) => num1*num2;
+const divide = (num1, num2) => num1/num2;
+
+
+
+console.log(add(1,1));
+console.log(subtract(1,1));
+console.log(multiply(1,1));
+console.log(divide(1,1));
