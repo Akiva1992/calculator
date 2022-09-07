2f3912ba82c230ef76f24d6bc2c6c93052621d72
Akiva1992 <breuerakiva@gmail.com>
2022-09-07 23:08:58 +0300

    created the run function to run the calculation and implement corrct math function

--- js/cal.js
+++ js/cal.js
@@ -1,11 +1,30 @@
+let num1 = +(prompt("num1?"));
+let operator = prompt("operator?");
+let num2 = +(prompt("num2?"));
+
 const add = (num1, num2) => num1+num2;
 const subtract = (num1, num2) => num1-num2;
 const multiply = (num1, num2) => num1*num2;
 const divide = (num1, num2) => num1/num2;
 
+const run = (num1, operator, num2) => {
+    if (operator==="+"){
+        return add(num1,num2);
+    }
+    else if (operator==="-"){
+        return subtract(num1,num2);
+    }
+    else if (operator==="*"){
+        return multiply(num1,num2);
+    }
+    else {
+        return divide(num1,num2);
+    }
+}
 
+// console.log(add(1,1));
+// console.log(subtract(1,1));
+// console.log(multiply(1,1));
+// console.log(divide(1,1));
 
-console.log(add(1,1));
-console.log(subtract(1,1));
-console.log(multiply(1,1));
-console.log(divide(1,1));
+console.log(run(num1,operator,num2));
