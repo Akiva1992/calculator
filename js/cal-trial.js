48c980031dff129168e98364dc6c058872201fb7
Akiva1992 <breuerakiva@gmail.com>
2022-09-12 23:19:44 +0300

    the cal works, I still need to do the js for the add btns like decimel off clear etc

--- js/cal-trial.js
+++ js/cal-trial.js
@@ -31,27 +31,24 @@ const operator = Array.from(document.querySelectorAll(".btn.operator"));
 const dot = document.querySelector(".btn.dot");
 const equals = document.querySelector(".btn.equals");
 
-let num1active = false, num2active = false, operatorActive = false, equalsActive= false; 
+let num1active = true, num2active = false, operatorActive = false, equalsActive= false; 
 let num1 = "", tempNum1="", num2 = "", operatorSym ="";
 
 /////////////////num1 and num2 event listeners////////////////////////////////////////
 numBtn.forEach(btn => {
     btn.addEventListener("click", ()=>{
-        const selected = operator.some(op => op.classList.contains("op-selected"));
-        if (!selected){
-            num2="";
-            result.innerHTML = "";
-            num2Container.innerHTML = "";
+        if (!operatorActive){
+            num1active = true;
+            num1Container.innerHTML ="";
+            num2Container.innerHTML ="";
             operatorContainer.innerHTML ="";
             num1 += btn.innerHTML
             console.log("num1: " + num1)
             num1Container.innerHTML = num1
             result.innerHTML = `=${num1}`
-            result.classList.remove("active");
-
         }
-        else if (selected && !result.classList.contains("active")){
-            num2Container.classList.add("num2-selected");
+        else if (operatorActive){
+            num2active = true;
             num2 += btn.innerHTML
             console.log("num2: " +num2)
             num2Container.innerHTML = num2
@@ -59,6 +56,54 @@ numBtn.forEach(btn => {
         }
     });
 }); 
+
+
+//////////////////////////////////////Operator listener/////////////////////////////////
+operator.forEach(op => {
+    op.addEventListener("click", ()=>{
+        if(num1active){
+            num1active = false;
+            operatorActive = true;
+            operatorContainer.innerHTML = op.innerHTML;
+            operatorSym = op.innerHTML;
+            console.log(operatorSym)
+        }
+        else if (!num1active && !num2active ){
+            num2active = false;
+            num1active = false;
+            operatorActive = true;
+            num1Container.innerHTML ="";
+            num2Container.innerHTML ="";
+            operatorContainer.innerHTML ="";
+            num1 = result.innerHTML.replace("=", "");
+            num1Container.innerHTML = num1
+            result.innerHTML = `=${num1}`
+            operatorContainer.innerHTML = op.innerHTML;
+            operatorSym = op.innerHTML;
+        }
+    });
+});
+
+////////////////////////////////////Equal listener/////////////////////////////////////////////
+equals.addEventListener("click", ()=>{
+    if (num2active){
+        num1active = false;
+        num2active = false;
+        operatorActive = false;    
+        // result.classList.add("active");
+        // problem.classList.add("inactive");
+        // operator.forEach(op=> op.classList.remove("op-selected"));
+        num1 = result.innerHTML.replace("=", "");
+        // num1Container.innerHTML ="";
+        // num2Container.innerHTML ="";
+        // operatorContainer.innerHTML ="";
+
+        num1 = "", num2="", operatorSym="";
+    }
+});
+
+
+
 
 /////////////////////////Floating point listener////////////////////////////
 dot.addEventListener("click", ()=>{
@@ -74,33 +119,6 @@ dot.addEventListener("click", ()=>{
         num2Container.innerHTML = num2
     }
 });
-
-//////////////////////////////////////Operator listener/////////////////////////////////
-operator.forEach(op => {
-    op.addEventListener("click", ()=>{
-        if(!num2Container.classList.contains("num2-selected")){
-            operatorContainer.innerHTML = op.innerHTML;
-            op.classList.add("op-selected");
-            dot.classList.add("dot-selected-num1");
-            operatorSym = op.innerHTML;
-            console.log(operatorSym)
-        }
-    });
-});
-
-////////////////////////////////////Equal listener/////////////////////////////////////////////
-equals.addEventListener("click", ()=>{
-    result.classList.add("active");
-    problem.classList.add("inactive");
-    operator.forEach(op=> op.classList.remove("op-selected"));
-    tempNum1 = result.innerHTML.replace("=", "");
-    num1 = "", num2="", operatorSym="";
-    num2Container.classList.remove("num2-selected");
-});
-
-
-
-
 
 
 
